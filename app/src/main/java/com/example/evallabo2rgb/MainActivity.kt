e911bbe88bda01957d3c879b38518400e3d44781
Xavizardon <00013812@uca.edu.sv>
2019-05-03 16:22:55 -0600

    Commit

---
+++ app/src/main/java/com/example/evallabo2rgb/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.evallabo2rgb
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
